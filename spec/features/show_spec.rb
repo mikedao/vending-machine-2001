b022d578502bfcb3610af93f42cdd1f917b06a04
David Tran <davidttemail@gmail.com>
2020-04-10 09:22:32 -0600

    Write tests for models

---
+++ spec/features/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe "vending machine show page" do
+  it "displays all of it's snacks as well" do
+    owner = Owner.create(name: "Sam's Snacks")
+    dons  = owner.machines.create(location: "Don's Mixed Drinks")
+    snack_1 = dons.snacks.create(name: "KitKat", price: 1)
+    snack_2 = dons.snacks.create(name: "Oreo", price: 1.5)
+    snack_3 = dons.snacks.create(name: "Chips", price: 2)
+
+    visit "/machines/#{dons.id}"
+
+    expect(page).to have_content("#{snack_1.name} - $#{snack_1.price}")
+    expect(page).to have_content("#{snack_2.name} - $#{snack_2.price}")
+    expect(page).to have_content("#{snack_3.name} - $#{snack_3.price}")
+
+  end
+end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -4,5 +4,7 @@ RSpec.describe Machine, type: :model do
   describe 'validations' do
     it { should validate_presence_of :location }
     it { should belong_to :owner }
+    it { should have_many :machine_snacks }
+    it { should have_many :snacks }
   end
 end
