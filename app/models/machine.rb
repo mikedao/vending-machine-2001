094588d67f9f41cbd9d2177d76da304cf1f100df
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-10 09:50:54 -0600

    US 2 complete

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  def average_snack_price
+    snacks.average(:price)
+  end
 end
