06787517435dc9315ee2e89f34bd7b806a5a5669
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-10 09:50:38 -0600

    Add test for average_snack_price method on machine model

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -8,5 +8,21 @@ RSpec.describe Machine, type: :model do
     it "has a valid factory" do
       expect(build(:machine)).to be_valid
     end
-  end  
+  end
+
+  describe 'methods' do
+    it "can calculate average price of all snacks" do
+
+      owner1 = create(:owner)
+      dons  = create(:machine, owner: owner1)
+
+      snack1 = create(:snack, price: 10.45)
+      snack2 = create(:snack, price: 4.53)
+      snack3 = create(:snack, price: 3.42)
+      snack4 = create(:snack, price: 6.33)
+
+      dons.snacks << [snack1, snack2, snack3, snack4]
+      expect(dons.average_snack_price.to_i).to eq(5)
+    end
+  end
 end
