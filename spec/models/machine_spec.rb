375c3e3a76608bb18dba1b4e9f022de6f5a3cec3
David Tran <davidttemail@gmail.com>
2020-04-10 09:27:58 -0600

    Add model test for calculating average

--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many :snacks }
   end
+
+  describe "methods" do
+    it "#average_price returns the average price of snacks" do
+      owner = Owner.create(name: "Sam's Snacks")
+      dons  = owner.machines.create(location: "Don's Mixed Drinks")
+      snack_1 = dons.snacks.create(name: "KitKat", price: 1)
+      snack_2 = dons.snacks.create(name: "Oreo", price: 1.5)
+
+      expect(dons.average_price).to eq(1.25)
+    end
+  end
 end
