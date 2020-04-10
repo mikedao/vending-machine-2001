7d7acb381363d92d6489ad926dd582ecfd29df96
David Tran <davidttemail@gmail.com>
2020-04-10 09:28:17 -0600

    Update MVC to display average price of snacks in a machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -4,4 +4,8 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  def average_price
+    snacks.average(:price).to_f
+  end
 end
